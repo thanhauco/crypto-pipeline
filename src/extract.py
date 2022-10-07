d5fef58f494677e6ed7056beaccc439f2a8b4261
Thanh Vu <thanhauco@gmail.com>
2022-10-07 09:42:00 -0700

    Add Ethereum Source

--- src/extract.py
+++ src/extract.py
@@ -1,3 +1,2 @@
-import requests
-def fetch_prices():
-    return requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd').json()
+def fetch_eth():
+    return requests.get('https://api.coingecko.com/api/v3/simple/price?ids=ethereum').json()
